ad755abcb82c0b1409de2a17d84d72804f25bd7b
Gayoung <gyyoon4u@naver.com>
2020-12-12 00:36:41 +0900

    feat: 지하철역 이름길이 에러처리

--- src/main/java/subway/domain/station/StationCheck.java
+++ src/main/java/subway/domain/station/StationCheck.java
@@ -2,11 +2,20 @@ package subway.domain.station;
 
 public class StationCheck {
 
-    private static final String NULL_ERROR_MESSAGE = "null 값을 입력하셨습니다.";
+    private static final String NULL_ERROR_MESSAGE = "[ERROR] null값을 입력하셨습니다.";
+    public static final String STATION_LENGTH_ERROR_MESSAGE = "[ERROR] 노선 이름은 2글자 이상 입력해야합니다.";
 
-    private void checkNull(String input) {
-        if (input == null) {
+    private static final int MIN_LENGTH_OF_STATION = 2;
+
+    private void checkNull(String station) {
+        if (station == null) {
             throw new IllegalArgumentException(NULL_ERROR_MESSAGE);
+        }
+    }
+
+    public void checkStationLength(String station) {
+        if (station.length() < MIN_LENGTH_OF_STATION) {
+            throw new IllegalArgumentException(STATION_LENGTH_ERROR_MESSAGE);
         }
     }
 }
