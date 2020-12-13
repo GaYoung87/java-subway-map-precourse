82abb060d8f03682b9c0cf994fc43dfc5c54c076
Gayoung <gyyoon4u@naver.com>
2020-12-13 15:28:23 +0900

    feat: 역, 노선 등록시 [info]제공

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -3,6 +3,9 @@ package subway.view;
 public class OutputView {
     private static final String MAIN_MENU = "## 메인 화면\n1. 역관리\n2. 노선 관리\n3. 구간 관리\n4. 지하철 노선도 출력\nQ. 종료\n";
     private static final String CHOOSE_COMMAND_MESSAGE = "## 원하는 기능을 선택하세요.";
+    private static final String INFO_MESSAGE = "[INFO] ";
+    private static final String STATION_SAVE_MESSAGE = "지하철 역이 등록되었습니다.";
+    private static final String LINE_SAVE_MESSAGE = "지하철 노선이 등록되었습니다.";
 
     public static void printMainMenu() {
         System.out.println(MAIN_MENU);
@@ -11,5 +14,13 @@ public class OutputView {
 
     public static void printChooseCommandMessage() {
         System.out.println(CHOOSE_COMMAND_MESSAGE);
+    }
+
+    public static void printStationSaveMessage() {
+        System.out.println(INFO_MESSAGE+STATION_SAVE_MESSAGE);
+    }
+
+    public static void printLineSaveMessage() {
+        System.out.println(INFO_MESSAGE+LINE_SAVE_MESSAGE);
     }
 }
