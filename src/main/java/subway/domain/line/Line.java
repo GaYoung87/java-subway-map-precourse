7763b66e80cf7f7c068d7fe5bd52f54645502ce6
Gayoung <gyyoon4u@naver.com>
2020-12-13 15:06:26 +0900

    feat: 노선등록을 위한 역 설정

--- src/main/java/subway/domain/line/Line.java
+++ src/main/java/subway/domain/line/Line.java
@@ -1,16 +1,26 @@
 package subway.domain.line;
 
+import subway.domain.station.Station;
+import subway.domain.station.StationRepository;
+
 import java.util.Objects;
 
 public class Line {
-    private final LineCheck lineName;
+    private final LineCheck lineCheck;
+    private final StationRepository stationRepository;
 
-    public Line(final String lineName) {
-        this.lineName = new LineCheck(lineName);
+    public Line(final String lineName, final String startStation, final String endStation) {
+        this.lineCheck = new LineCheck(lineName);
+        this.stationRepository = new StationRepository().addStation(startStation).addStation(endStation);
+    }
+
+    public Line(final LineCheck lineCheck, final StationRepository stationRepository) {
+        this.lineCheck = lineCheck;
+        this.stationRepository = stationRepository;
     }
 
     public String getName() {
-        return lineName.getName();
+        return lineCheck.getName();
     }
 
     @Override
--- src/main/java/subway/domain/station/StationRepository.java
+++ src/main/java/subway/domain/station/StationRepository.java
@@ -1,30 +1,35 @@
 package subway.domain.station;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class StationRepository {
-    private static final List<Station> stations = new ArrayList<>();
+    private static List<Station> stations = new ArrayList<>();
     private final String DUPLICATE_STATION_ERROR_MESSAGE = "[ERROR] 이미 등록된 역 이름입니다. 중복되지 않는 역이름을 입력해주세요.";
     private static final String INFO="[INFO] ";
+
+    public StationRepository() {
+        this.stations = new LinkedList<>();
+    }
+
+    public StationRepository(final List<Station> stations) {
+        this.stations = stations;
+    }
 
     public List<Station> stations() {
         return Collections.unmodifiableList(stations);
     }
 
-    public boolean addCheck(Station station) {
-        if (addStation(station)){
-            stations.add(station);
-            return true;
-        }
-        return false;
+    public StationRepository addStation(String station) {
+        return add(stations.size(), station);
     }
 
-    public boolean addStation(Station station) {
+    public StationRepository add(int index, String station) {
         if (stations.contains(station)) {
             System.out.println(DUPLICATE_STATION_ERROR_MESSAGE);
-            return false;
         }
-        return true;
+        stations.add(index, new Station(station));
+        return new StationRepository(stations);
     }
 
     public boolean deleteStation(String name) {
